6c36852f00149a633b3cda2aea9cc462a614ee82
Cauê Gonzalez <cauegonzalez@gmail.com>
2020-06-20 09:33:47 -0300

    Adding the AdminLTE template

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
